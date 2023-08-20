4ce04049bed7692ce3416c93a33cbd8e8eafdaf8
Jan Krupiński <jk.krupinski@gmail.com>
2023-08-20 17:21:00 +0200

    [rzeszow] Sort cities in menu

--- src/pages/Layout/Menu/Menu.tsx
+++ src/pages/Layout/Menu/Menu.tsx
@@ -1,14 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 
-import { cities } from 'api';
+import API, { QUERY } from 'api';
 
 import * as Styled from './Menu.styles';
 
 const Menu = () => {
   const { t } = useTranslation('cities');
+
+  const { data = [] } = useQuery([QUERY.CITIES_LIST], API.getCitiesList, {
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <Styled.Menu>
-      {cities.map((city) => (
+      {data.map(({ id: city }) => (
         <Styled.MenuItem key={city}>
           <Styled.MenuLink to={`/city/${city}`} checkCurrent>
             <Styled.MenuImg src={`${import.meta.env.BASE_URL}/assets/coa/city/${city}.svg`} />
